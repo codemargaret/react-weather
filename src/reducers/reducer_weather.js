b79b23638b4a9068a1ad5f59691a6594b8e5108b
Margaret Berry <codeberry1@gmail.com>
2018-02-23 16:33:54 -0800

    fix 404 error

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,9 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
   case FETCH_WEATHER:
-    return [ action.payload.data, ...state ];
+    if(!action.error) {
+      return [ action.payload.data, ...state ];
+    }  
   }
   return state;
 }
